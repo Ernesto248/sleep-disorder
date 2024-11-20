92c57da402ff43d597c7a78efd36e7ac3548793c
Jenifer Casalis Chau <jenicasalis04@icloud.com>
2024-11-20 12:21:53 -0500

    Last changes

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,11 +5,11 @@ function Home() {
     <Layout>
       <div className="flex flex-col md:flex-row md:items-center items-start justify-center m-auto h-auto">
         <div className="w-full md:w-1/2">
-          <img src="slep.jpg" alt="" className="w-full h-auto rounded-lg" />
+          <img src="img2.jpeg" alt="" className="w-full h-auto rounded-lg" />
         </div>
         <div className="w-full md:w-1/2 flex flex-col items-center justify-center text-center m-auto mt-4 md:mt-0">
           <h1 className="text-3xl font-bold">
-            Trastorno del sueño en pacientes pediatricos
+            Evaluación de pacientes pediátricos con trastornos del sueño
           </h1>
           <p className="text-xl mt-4">
             Esta aplicación utiliza un modelo de inteligencia artificial para
@@ -20,8 +20,7 @@ function Home() {
           <p className="text-xl mt-4">
             <strong>Creado por:</strong>
             <br />
-            Dra. Grethel Camejo Sampedro <br />
-            José Daniel Monzón Rodríguez
+            Dra. Grethel Camejo Sampedro
           </p>
         </div>
       </div>
